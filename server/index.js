4d6bbd8a7ad43afc2c395939c9388802f9e9acb8
phc5 <paulhyunchong@gmail.com>
2016-12-12 10:16:47 -0800

    update gitignore to ignore .env

--- server/index.js
+++ server/index.js
@@ -57,6 +57,7 @@ app.get('/auth/google', passport.authenticate('google', {scope:['profile']}));
 
 app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: 'login'}),
     (req, res) => {
+        // Successful authentication, redirect home
         res.redirect('/');
     }
 );
