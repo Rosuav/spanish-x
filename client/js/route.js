fe3038454dd8579745e482dfb996b95a8260547f
emontecalvo <emilymontecalvo@gmail.com>
2016-12-12 08:54:49 -0800

    added boilerplate index, route and store plus component/action/reducer folders

---
+++ client/js/route.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+//import Login from './components/Login';
+import App from './components/app';
+
+// attribute on Route onEnter= 
+// function if user does not exists, route back to login
+
+export default (
+	<Route path='/' component={App}>
+		<IndexRoute component={Login} />
+		<Route path='app' component={App} />
+		<Route path='login' component={Login} />
+	</Route>
+);
+
+
